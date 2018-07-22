935894c0b7e02e36e32fc1812c2c59314755b266
Dasha <37613468+dashahsh@users.noreply.github.com>
2018-07-23 01:08:16 +0300

    upd

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 // Global app controller
 import Search from './models/Search';
+import { elements, renderLoader, clearLoader } from "./view/base";
+import * as searchView from './view/searchView';
 
 /** Global app state
  * - Search object
@@ -11,23 +13,38 @@ const state = {};
 
 const controlSearch = async () => {
     // 1. get data from view
-    const query = 'pasta';
+    const query = searchView.getSearchInputValue();
 
     if (query) {
         // 2. create new Search object
         state.search = new Search(query);
 
         // 3. set UI for results
+        searchView.clearForm();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
 
         // 4. provide search
         await state.search.getResult();
 
-        console.log(state.search.result);
+        // 5. render result
+        searchView.renderResult(state.search.result);
+        clearLoader();
     }
 };
 
 // Set events
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto);
+        searchView.clearResults();
+        searchView.renderResult(state.search.result, goToPage);
+    }
 });
